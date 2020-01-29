081a38f482ca3c736a69141d62f13b3975f7ece2
Arasou-0206 <chso17040@g.nihon-u.ac.jp>
2020-01-29 12:01:10 +0900

    chenge letter

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const App = () => {
     })
   },[])
   const newData = data.filter(function(item, index){
-    if(item.likes > 100 && item.words > 10000)return true;
+    if(item.likes >= 100 && item.words >= 10000)return true;
   });
 
   return (
@@ -23,8 +23,15 @@ const App = () => {
             <div className="content has-text-centered">
               <h3>2019年度尾上ゼミ 情報科学講究1 課題制作</h3>
               <h1>Qiitaの『いいね』数と文字数に関する可視化</h1>
-              <p>これはデータの説明です。</p>
-              <p>これは可視化の説明です。</p>
+              <h4>使用データ</h4>
+                <p>今回可視化したのは,Qiitaに投稿された記事に対して与えられた『いいね』の数と,その投稿の文字数を集計したものです.</p>
+                <p>データ内には,記事のID,記事につくタグ,文字数,いいねの数が格納されています.</p>
+              <h4>可視化手法</h4>
+                <p>JavaScriptを用いてグラフの作成をおこないました.</p>
+                <p>収集されたデータはデータ数が多く可視化に影響するため,ある程度のデータ削除を行いました.</p>
+                <p>今回は『いいね』数は100以上かつ文字数は10000文字以上のものを可視化しました.</p>
+                <p>削減したデータ をもとに,d3に構築されたdatavizアプリを構築するためのReactコンポーネントnivoを用いて線及び点を表示しました.</p>
+                <p>タグ内の文字数を円のサイズに,『いいね』数を縦軸に取りました.</p>
               <div div style={{width: '1350px', height: '1300px' , overflow:'auto'}} >
                <div style={{width: '3500px', height: '1200px'}} >
     <ResponsiveSwarmPlotCanvas
@@ -85,7 +92,7 @@ const App = () => {
                </div>
               </div>
               <br></br>
-              <p>これは可視化結果の考察です。</p>
+              <p>これは可視化結果の考察です.</p>
             </div>
           </div>
         </section>
